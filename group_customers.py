6da6a50ab034d1b4d7ae6e526aeefd3121aa8632
Varsha <varshabalasubramaniyan7@gmail.com>
2025-12-19 08:53:51 +0530

    updataedd

--- group_customers.py
+++ group_customers.py
@@ -2,12 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
-
 data = pd.read_csv("Mall_Customers.csv")
 
-
 X = data[['Annual Income (k$)', 'Spending Score (1-100)']]
-
 
 wcss = []
 for i in range(1, 11):
@@ -21,17 +18,14 @@ plt.xlabel("Number of Clusters")
 plt.ylabel("WCSS")
 plt.show()
 
-
 kmeans = KMeans(n_clusters=5, init='k-means++', random_state=42)
 y = kmeans.fit_predict(X)
-
 
 plt.scatter(X.iloc[y == 0, 0], X.iloc[y == 0, 1], s=50, label='Cluster 1')
 plt.scatter(X.iloc[y == 1, 0], X.iloc[y == 1, 1], s=50, label='Cluster 2')
 plt.scatter(X.iloc[y == 2, 0], X.iloc[y == 2, 1], s=50, label='Cluster 3')
 plt.scatter(X.iloc[y == 3, 0], X.iloc[y == 3, 1], s=50, label='Cluster 4')
 plt.scatter(X.iloc[y == 4, 0], X.iloc[y == 4, 1], s=50, label='Cluster 5')
-
 
 plt.scatter(kmeans.cluster_centers_[:, 0],
             kmeans.cluster_centers_[:, 1],
